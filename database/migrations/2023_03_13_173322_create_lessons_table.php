e62164daa4b442546c0c275150057002dc5901f2
Izaias Neto <izaiasmneto@gmail.com>
2023-03-13 15:06:39 -0300

    Support and ReplySupport Table

--- database/migrations/2023_03_13_173322_create_lessons_table.php
+++ database/migrations/2023_03_13_173322_create_lessons_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('lessons', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->uuid('module_id');
-            $table->string('name');
+            $table->uuid('module_id')->index();
+            $table->string('name')->unique();
             $table->string('url')->unique();
             $table->string('video')->unique();
             $table->text('description')->nullable();
