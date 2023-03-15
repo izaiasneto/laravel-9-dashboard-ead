a1fd5ac9988f82dd817f63af8c575d6db9340c1e
Izaias Neto <izaiasmneto@gmail.com>
2023-03-15 12:04:49 -0300

    List Modules by Course

--- app/Http/Controllers/Admin/ModuleController.php
+++ app/Http/Controllers/Admin/ModuleController.php
@@ -5,20 +5,29 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Services\{
+    CourseService,
     ModuleService,
 };
 
 class ModuleController extends Controller
 {
     protected $repository;
+    protected $repositoryCourse;
 
-    public function __construct(ModuleService $repository)
+    public function __construct(CourseService $repositoryCourse, ModuleService $repository)
     {
         $this->repository = $repository;
+        $this->repositoryCourse = $repositoryCourse;
     }
 
     public function index($courseId)
     {
-       $this->repository->getAllByCourseId($courseId);
+       if (!$course = $this->repositoryCourse->findById($courseId))
+            return back();
+
+       $data = $this->repository->getAllByCourseId($courseId);
+       $modules = convertItemsOfArrayToObject($data);
+
+       return view('admin.courses.modules.index', compact('course', 'modules'));
     }
 }
