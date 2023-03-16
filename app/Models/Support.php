4e527b036ac35815375b56d00a9fa133e4c65cf8
Izaias Neto <izaiasmneto@gmail.com>
2023-03-16 09:07:11 -0300

    List Reply Support

--- app/Models/Support.php
+++ app/Models/Support.php
@@ -29,6 +29,11 @@ class Support extends Model
     {
         return $this->belongsTo(Lesson::class);
     }
+
+    public function replies()
+    {
+        return $this->hasMany(ReplySupport::class);
+    }
 }
    
 
